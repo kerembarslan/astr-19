b4dc42015a9d55666823f6a5629194d839bf021e
kerembarslan <karslan@ucsc.edu>
2023-10-17 22:56:30 -0700

    Session 1 Prompt.py

---
+++ Session 1 Prompt.py
@@ -0,0 +1,20 @@
+# Session 1 Prompt.py
+# Name, Pronouns, and Favorite Movie and Food
+ 
+
+# basic variables assigned
+
+name = "Kerem Burak Arslan"
+pronoun = "He/Him"
+
+# complicated variables assigned
+
+fav_movie = "My favorite movie is Kung-Fu Panda. I am a big fan of Jack Black"
+fav_food = "My favorite food is Grilled Cheese, I like eating it in the morning with eggs"
+
+#Combine all variables to single variable and add bullet points
+#Split name and pronoun from favorites and favorites from each other with n\
+
+full_text= "Name: "+name+" Pronouns: "+pronoun+" \n\nFavorite Movie: "+fav_movie+" \nFavorite Food: "+fav_food
+
+print(full_text)
