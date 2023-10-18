56f258b0adb8779eac697936f90aa7d22cda95b1
kerembarslan <karslan@ucsc.edu>
2023-10-18 00:51:32 -0700

    Session 2 Prompt.py

---
+++ Session 2 Prompt.py
@@ -0,0 +1,21 @@
+# Session 2 Prompt.py 
+
+# Input the float and integer number values
+
+float_num1 = 8.0
+float_num2 = 4.3
+
+int_num1 = 30
+int_num2 = 7
+
+# Name the final result variable
+
+question1result = float_num1 + float_num2
+question2result = int_num2 - int_num1
+question3result = int_num2 * float_num1
+
+# Enter equations, have results print out thr type of the result
+
+print("type:",type(question1result), question1result)
+print("type:",type(question2result), question2result)
+print("type:",type(question3result), question3result)
