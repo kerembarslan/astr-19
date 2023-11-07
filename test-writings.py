5039ad08c60c40994bcc95cd2564d734ee6f64b2
kerembarslan <karslan@ucsc.edu>
2023-11-07 15:31:55 -0800

    Week_2_Sessions.py

---
+++ test-writings.py
@@ -0,0 +1,167 @@
+	import numpy as np 
+	import sys
+
+	def main():
+		x = 0.0
+		for i in range(20):
+			x = float(2*i + 19)
+			print(f"value of x is =v{x}.")
+
+	if __name__=="__main__":
+		main()
+
+
+
+
+	import numpy as np 
+	import sys
+
+	def main():
+		x = 0.0 
+		for i in range(20):
+			x = float(2*i + 19)
+			print(f"value of x = {x}.")
+
+	if __name__=="__main__":
+		main()
+
+
+
+		import numpy as np 
+		import sys 
+
+		def main():
+			x = 0.0
+			for i in range(20):
+				x = float(2*i + 19)
+				print(f"value of x = {x}.")
+
+		if __name__=="__main__":
+			main()
+
+
+
+		import numpy as np
+		import sys 
+
+			def main():
+				x = 0.0
+				for i in range(20):
+					x = float(2*i + 19)
+					print(f"value of x = {x}.")
+			
+			if __name__=="__main__":
+				main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+		import numpy as np 
+		import sys
+
+		def main():
+			x = 0.0
+			for i in range(20):
+				x = float(2*i + 19)
+				print (f"value of x = {x}.")
+
+
+		if __name__=="__main__":
+			main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import numpy as np 
+import sys
+
+def main():
+	x = 0.0
+	for i in range(20):
+		x = float(2*i + 19)
+		print(f"The value of x is = {x}")
+
+if __name__=="__main__":
+	main()
+
+
+		
